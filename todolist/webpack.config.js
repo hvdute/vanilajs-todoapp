55a7212ed59e6c312fd06fa13335df75d0e24a0f
Dung Hoang <dunghv@dgroup.co>
2017-12-14 18:06:33 +0700

    Added HMR and inline-source-map

--- todolist/webpack.config.js
+++ todolist/webpack.config.js
@@ -1,4 +1,5 @@
 const path = require('path');
+const Webpack = require('webpack');
 
 module.exports = {
   entry: "./js/main.js",
@@ -7,11 +8,16 @@ module.exports = {
     filename: 'bundle.js',
     publicPath: '/dist/'
   },
+  devtool: 'inline-source-map',
+  plugins: [
+    new Webpack.NamedModulesPlugin(),
+    new Webpack.HotModuleReplacementPlugin()
+  ],
   devServer: {
     // contentBase: path.resolve(__dirname),
     // filename: 'bundle.js',
     // publicPath: 'http://localhost:9000/dist/',
     port: 9000,
-    // hot: true
+    hot: true
   }
 }
